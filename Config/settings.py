773b3475554ff414d001ebc783b4b1c6a49a4c94
askvart <tur1amant@gmail.com>
2021-04-26 16:34:31 +0300

    django-heroku delette

--- Config/settings.py
+++ Config/settings.py
@@ -112,10 +112,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_FINDERS = [
-    "django.contrib.staticfiles.finders.FileSystemFinder",
-    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
-]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
