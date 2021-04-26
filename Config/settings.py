cdd2b3f6b2678c0d6fcddd2b418f3fdc0291b085
askvart <tur1amant@gmail.com>
2021-04-26 16:22:49 +0300

    django-heroku delette

--- Config/settings.py
+++ Config/settings.py
@@ -1,9 +1,8 @@
-import django_heroku
 import os.path
 from pathlib import Path
 
 # Activate Django-Heroku.
-django_heroku.settings(locals())
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
