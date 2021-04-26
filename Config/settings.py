a7c32f22b11e93f1264aaa9d7b72aaf497acff65
askvart <tur1amant@gmail.com>
2021-04-26 22:50:55 +0300

    sdcd

--- Config/settings.py
+++ Config/settings.py
@@ -7,6 +7,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import dj_database_url
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -73,7 +75,8 @@ DATABASES = {
         'PORT': 5432,
     }
 }
-
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
